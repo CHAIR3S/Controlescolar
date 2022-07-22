6b7ba17d9bcb759fac6924ae4f36745222fea373
luisf <luisf@192.168.1.79>
2022-07-21 19:41:42 -0500

    JpaRepositorys Interfaces Modelos

---
+++ src/main/java/com/utng/controlescolar/repository/ILoginJpaRepository.java
@@ -0,0 +1,7 @@
+package com.utng.controlescolar.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ILoginJpaRepository extends JpaRepository <ILoginJpaRepository, Integer>{
+
+}
---
+++ src/main/java/com/utng/controlescolar/repository/IRolJpaRepository.java
@@ -0,0 +1,9 @@
+package com.utng.controlescolar.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.utng.controlescolar.model.Rol;
+
+public interface IRolJpaRepository extends JpaRepository <Rol, Integer>{
+
+}
